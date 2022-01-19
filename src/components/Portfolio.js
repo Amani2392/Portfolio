a8a9bcc1613774069e9f6f14b0c3654ea9985beb
amani2392~ <amani.babahaj@gmail.com>
2022-01-19 18:54:46 +0100

    responsive ish

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,10 @@ import React from "react";
 import "../css/Portfolio.scss";
 
 const Portfolio = () => {
-  return <div className="portfolio" id="portfolio"></div>;
+  return <div className="portfolio" id="portfolio">
+    <div className="right"></div>
+    <div className="left"></div>
+  </div>;
 };
 
 export default Portfolio;
