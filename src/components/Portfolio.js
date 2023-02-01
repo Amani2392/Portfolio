20637dbba6e66f76fe1f7880a620abad103b8d4c
Amani Baba Haj <86953119+Amani2392@users.noreply.github.com>
2023-02-01 16:11:35 +0100

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,7 +7,7 @@ const Portfolio = () => {
       <div className="left">
         <img className="my-pic" src="assets/my-pic.png" alt=""></img>
         <a
-          href="assets/cv Amani Baba haj.pdf"
+          href="assets/cv.pdf"
           target="_blank"
           class="button-53"
         >
